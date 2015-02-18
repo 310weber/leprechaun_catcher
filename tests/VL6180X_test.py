d47bce26fdfdf89edee1eeb8531fe60a05deb318
Arnie Weber <arnieweb@gmail.com>
2015-02-17 21:34:13 -0500

    ADded command line argument for debug on/off

--- tests/VL6180X_test.py
+++ tests/VL6180X_test.py
@@ -9,13 +9,12 @@ from ST_VL6180X import VL6180X
 from time import sleep
 import RPi.GPIO as GPIO  # Import GPIO functions
 
+debug = False
 if len(sys.argv) > 1:
     if sys.argv[1] == "debug":  # sys.argv[0] is the filename
         debug = True
-else:
-    debug = False
 
-# setup i2c bus and ToF address
+# setup ToF ranging/ALS sensor
 tof_address = 0x29
 tof_sensor = VL6180X(address=tof_address, debug=debug)
 tof_sensor.get_identification()
@@ -32,6 +31,7 @@ if tof_sensor.debug:
 tof_sensor.default_settings()
 
 # Set output pin numbers for LEDS
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)  # Use GPIO numbering scheme (not pin numbers)
 LED = [17, 27]  # List of GPIOs to use for LED output
 
